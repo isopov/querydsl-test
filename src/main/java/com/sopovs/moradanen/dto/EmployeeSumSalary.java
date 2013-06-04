d9169fcfc4b928c198b2207d234b5004e8880be5
Ivan Sopov <moradan228@gmail.com>
2013-06-05 00:27:43 +0300

    cumulative sum using querydsl implemented

---
+++ src/main/java/com/sopovs/moradanen/dto/EmployeeSumSalary.java
@@ -0,0 +1,59 @@
+package com.sopovs.moradanen.dto;
+
+import com.mysema.query.annotations.QueryProjection;
+
+public class EmployeeSumSalary {
+    private final String name;
+    private final Long value;
+
+    @QueryProjection
+    public EmployeeSumSalary(String name, Long value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getValue() {
+        return value;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        EmployeeSumSalary other = (EmployeeSumSalary) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (value == null) {
+            if (other.value != null)
+                return false;
+        } else if (!value.equals(other.value))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeeSumSalary [name=" + name + ", value=" + value + "]";
+    }
+
+}
